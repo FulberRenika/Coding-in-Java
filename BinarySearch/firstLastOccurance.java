54cfffff91bdc398a933634336a4e632cbf5d6aa
Fulber Renika C R <fulberrenika@gmail.com>
2026-09-16 07:53:04 +0000

    "Complete java code"

--- BinarySearch/firstLastOccurance.java
+++ BinarySearch/firstLastOccurance.java
@@ -9,22 +9,25 @@ public class firstLastOccurance{
             int mid=left+(right-left)/2;
             if(arr[mid]>=target){
                 first=mid;
-                right=mid;
+                right=mid-1;
             }
             else{
                 left=mid+1;
             }
         }
+        left=0;
+        right=arr.length-1;
         while(left<right){
             int mid=left+(right-left)/2;
             if(arr[mid]<=target){
                 last=mid;
-                left=mid;
+                left=mid+1;
             }
             else{
                 right=mid-1;
             }
         }
+        return new int[]{first,last};
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -34,6 +37,6 @@ public class firstLastOccurance{
             arr[i]=sc.nextInt();
         }
         int target=sc.nextInt();
-        System.out.println(firstLastOccurance(arr,target));
+        System.out.println(Arrays.toString(firstLastOccurance(arr,target)));
     }
 }
